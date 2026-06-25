e0cc6a5f19d76bcf0a2fec826290d46555fc92c8
Benhaha-adam <benhahaadam@gmail.com>
2026-06-25 15:56:53 +0100

    streamlit

---
+++ test.py
@@ -0,0 +1,25 @@
+# app.py
+import test as st
+import joblib
+import pandas as pd
+import numpy as np
+
+st.title("🚗 AutoPredict - Car Price Estimator")
+
+model = joblib.load("./models/random_forest.joblib")
+
+year = st.number_input("Year", 1990, 2026, 2018)
+mileage = st.number_input("Mileage", 0, 300000, 50000)
+hp = st.number_input("Engine HP", 50, 1500, 150)
+
+if st.button("Predict"):
+    data = pd.DataFrame([{
+        "year": year,
+        "mileage": mileage,
+        "engine_hp": hp
+    }])
+
+    pred = model.predict(data)[0]
+    price = np.expm1(pred)
+
+    st.success(f"Estimated price: ${price:,.0f}")
